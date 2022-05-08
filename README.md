207752dad01f1b839de97baa29f824831f7e9ac3
debil03311 <andrei.hent@yahoo.com>
2022-05-08 18:55:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# dangeru.js
+
+An asynchronous JavaScript API wrapper for the [danger/u/](https://dangeru.us/) textboard.
+
+## Example usage
+
+Import the module
+
+```javascript
+const dangeru = require("dangeru-js");
+```
+
+Get the available boards
+
+**NOTE:** This data is scraped from the front page since the API doesn't provide the full board names
+
+```javascript
+const boards = await dangeru.getBoards();
+```
+```json
+[
+  {
+    short: "a",
+    long: "Anime & Manga"
+  },
+  ...boards
+]
+```
+
+Get threads from the third page of the Technology (`/tech/`) board. The page index can be omitted and defaults to `0`.
+
+```javascript
+const threads = await dangeru.getThreads("tech", 2);
+```
+```json
+[
+  {...threadObject},
+  ...threads
+]
+```
+
+Get the replies to thread no. 376318
+
+```javascript
+const replies = await dangeru.getReplies(376318);
+```
+```json
+[
+  {...replyObject},
+  ...replies
+]
+```
+
+Get the amount of pages that the `/u/` board has
+
+**NOTE:** This data is scraped from the front page since it's not provided by the API
+
+```javascript
+const threads = await dangeru.getPageCount("u");
+```
+```json
+5
+```
+
+Get the front page statistics
+
+**NOTE:** This data is scraped from the front page since it's not provided by the API
+
+```javascript
+const stats = await dangeru.getStats();
+```
+```json
+{
+  threads: {
+    active: 294,
+    archived: 96801,
+  },
+  replies: {
+    active: 8281,
+    archived: 698258,
+  },
+  burgs: {
+    normal: 51937,
+    angry: 53127,
+  }
+}
+```
+
+Get the front page news announcement
+
+**WARNING:** This may break if the date format in the announcement changes in the future
+
+```javascript
+const news = await dangeru.getNews();
+```
+```json
+{
+  announcement: "Song-chan's in trouble! If you can spare it, please help: https://dangeru.us/u/thread/828545",
+  dateRaw: "18/02/22",
+  date: Fri Feb 18 2022 00:00:00 GMT+0200 (Eastern European Summer Time)
+}
+```
---
+++ index.js
@@ -0,0 +1,185 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const baseUrl = "https://dangeru.us";
+const apiEndpoint = `${baseUrl}/api/v2`;
+  
+/**
+ * Make a GET request to the danger/u/ API
+ * @async
+ * @param {String} path - API path
+ * @returns {JSON}
+ */
+async function apiFetch(path) {
+  return await axios.get(apiEndpoint + path)
+    .catch(console.error)
+}
+
+/**
+ * @async
+ * @returns {{
+ *   date: Date,
+ *   dateString: String,
+ *   announcement: String
+ * }}
+ */
+async function getNews() {
+  const html = await axios.get(baseUrl)
+    .then((response)=> response.data)
+
+  const $ = cheerio.load(html);
+  const redtext = $("#index-news .redtext").text();
+
+  // The date should be the first thing in the announcement
+  // and follow the format of "DD/MM/YY".
+
+  // Should. Pray that it is.
+
+  const dateString = redtext
+    .match(/[\w\d\/]+/)[0]
+
+  // Flip date around
+  const dateArray = dateString
+    .split("/")
+    .reverse()
+
+  // If for some reason something's missing from the date
+  // prepend the current year to it
+  if (dateArray.length < 3)
+    dateArray.unshift(new Date().getFullYear);
+
+  // Better safe than sorry right
+  const yearLength = dateArray[0].length;
+
+  // Literally shaking and crying rn
+  if (yearLength < 4) {
+    const yearPrefix = new Date()
+      .getFullYear()
+      .toString()
+      .slice(0, 4 - yearLength)
+
+    dateArray[0] = yearPrefix + dateArray[0];
+  }
+
+  // Please work I don't want to put like 3 entire
+  // try-catch blocks in here please
+  return {
+    date: new Date(dateArray.join("/")) || undefined,
+    dateRaw: dateString,
+    announcement: redtext.match(/\s(.*)/)[1],
+  }
+}
+
+/**
+ * @async
+ * @returns {{
+ *   threads: {
+ *     active: Number,
+ *     archived: Number,
+ *   },
+ *   replies: {
+ *     active: Number,
+ *     archived: Number,
+ *   },
+ *   burgs: {
+ *     normal: Number,
+ *     angry: Number,
+ *   }
+ * }}
+ */
+async function getStats() {
+  const html = await axios.get(baseUrl)
+    .then((response)=> response.data)
+
+  const $ = cheerio.load(html);
+
+  const numbers = $(".comment-styled.boarda-margin.index-links span[style='font-weight: bold;']")
+    .map((_, span)=> parseInt( $(span).text() ))
+
+  return {
+    threads: {
+      active: numbers[0],
+      archived: numbers[2],
+    },
+    replies: {
+      active: numbers[1],
+      archived: numbers[3],
+    },
+    burgs: {
+      normal: numbers[4],
+      angry: numbers[5],
+    }
+  }
+}
+
+/**
+ * @async
+ * @returns {Array.<{full: String, short: String}>} 
+ */
+async function getBoards() {
+  // You can only retrieve the short board names
+  // from the API, so let's scrape them from the
+  // front page instead.
+
+  // const boards = await apiFetch("/boards")
+  //   .then((response) => response.data)
+
+  const html = await axios.get(baseUrl)
+    .then((response)=> response.data)
+
+  const $ = cheerio.load(html);
+
+  const boards = $("#index-boards a")
+    .map((_, anchor)=> ({
+      long: $(anchor).text(),
+      short: anchor.attribs.href
+        .match(/(?!\/)[^\/]+/g)
+        .reverse()[0],
+    }))
+
+  return boards;
+}
+
+/**
+ * @async
+ * @param {String} board
+ * @param {Integer} pageNumber
+ * @returns {Object[]} Array of thread objects from the given board
+ */
+async function getThreads(board, pageNumber=0) {
+  return await apiFetch(`/board/${board}?page=${pageNumber}`)
+    .then((response) => response.data)
+}
+/**
+ * @async
+ * @param {String} board
+ * @returns {Object[]} Array of thread objects from the given board
+ */
+async function getPageCount(board) {
+  const html = await axios.get(`${baseUrl}/${board}`)
+    .then((response)=> response.data)
+
+  const $ = cheerio.load(html);
+
+  return parseInt(
+    $(".pagecount").last().text())
+}
+
+/**
+ * @async
+ * @param {Number} threadId
+ * @returns {Object[]} Array of replies to the given thread ID
+ */
+async function getReplies(threadId) {
+  return await apiFetch(`/thread/${threadId}/replies`)
+    .then((response) => response.data)
+}
+
+module.exports = {
+  apiFetch: apiFetch,
+  getNews: getNews,
+  getBoards: getBoards,
+  getThreads: getThreads,
+  getReplies: getReplies,
+  getPageCount: getPageCount,
+}
